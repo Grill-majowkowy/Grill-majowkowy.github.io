f761cdfb99665e19a1e53f51245008fc5a0b5d2c
bartoszm <bartoszmm9876512345@gmail.com>
2026-05-13 11:14:53 +0200

    Display user photos in gallery

--- src/views/GaleryPage/GaleryPage.vue
+++ src/views/GaleryPage/GaleryPage.vue
@@ -34,7 +34,8 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import { IonCol, IonGrid, IonRow } from '@ionic/vue';
 import PageHeader from '../../components/PageHeader.vue';
-import { ref, onMounted, onActivated } from 'vue'
+import { ref, onMounted, onActivated } from 'vue';
+import { Filesystem, Directory } from "@capacitor/filesystem";
 
 const size = ref(6); // Standardowy rozmiar kolumny dla zwykłego ekranu telefonu
 const sizeSm = ref(4); // Rozmiar kolumny gdy ekran jest większy niż 576px
@@ -43,15 +44,56 @@ const sizeLg = ref(4); // Rozmiar kolumny gdy ekran jest większy niż 992px
 const sizeXl = ref(3); // Rozmiar kolumny gdy ekran jest większy niż 1200px
 
 const images = ref([])
+const capacitorPlatform = Capacitor.getPlatform()
+const isNative = capacitorPlatform === 'android' || capacitorPlatform === 'ios';
 
-async function loadImages() {
+async function loadDefaultImages() {
   const res = await fetch('/pictures/manifest.json');
   const files = await res.json();
-  images.value = files.map((pictureFile, i) => ({ id: i + 1, src: `/pictures/${pictureFile}` }));
+  files.forEach((pictureFile, i) => {
+    images.value.push({ id: i + 1, src: `/pictures/${pictureFile}` })
+  });
 }
 
-onMounted(loadImages)
-onActivated(loadImages)
+const getPhoto = async (photoName) => {
+  if (isNative) {
+    const { uri } = await Filesystem.getUri({
+      path: photoName,
+      directory: Directory.Data,
+    });
+    return Capacitor.convertFileSrc(uri); // zamienia na webowy URL
+  } else {
+    const result = await Filesystem.readFile({
+      path: photoName,
+      directory: Directory.Data,
+    });
+    return `data:image/jpeg;base64,${result.data}`;
+  }
+};
+
+async function loadUserImages(){
+  try {
+    const { files } = await Filesystem.readdir({
+      path: 'photos',
+      directory: Directory.Data,
+    });
+    const sortedFiles = files.sort((a, b) => b.name.localeCompare(a.name));
+    for (const file of sortedFiles) {
+      const src = await getPhoto(`photos/${file.name}`);
+      images.value.push({ id: Date.now() + Math.random(), src });
+    }
+  } catch {
+    console.error("Nie można odczytać zdjęć z katalogu.");
+  }
+}
+
+async function loadAllImages() {
+  await loadUserImages();
+  await loadDefaultImages();
+}
+
+onMounted(loadAllImages);
+// onActivated(loadDefaultImages)
 </script>
 
 <style scoped>
