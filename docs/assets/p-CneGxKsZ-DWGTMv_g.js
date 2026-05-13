c7829313379f834fb7edf47ac5d7884735f1e89e
bartoszm <bartoszmm9876512345@gmail.com>
2026-05-13 14:09:44 +0200

    build

--- docs/assets/p-CneGxKsZ-DWGTMv_g.js
+++
@@ -1,3 +0,0 @@
-import{ab as o,ac as s,ad as r,ae as i,af as d}from"./index-D4rlXm26.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const c=()=>{const t=window;t.addEventListener("statusTap",()=>{o(()=>{const n=document.elementFromPoint(t.innerWidth/2,t.innerHeight/2);if(!n)return;const e=s(n);e&&new Promise(a=>r(e,a)).then(()=>{i(async()=>{e.style.setProperty("--overflow","hidden"),await d(e,300),e.style.removeProperty("--overflow")})})})})};export{c as startStatusTap};
