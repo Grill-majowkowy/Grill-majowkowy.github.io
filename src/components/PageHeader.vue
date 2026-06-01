ae08bb6f45640807aa6a6292ca5c412068baeb1d
bartoszm <bartoszmm9876512345@gmail.com>
2026-06-01 19:37:53 +0200

    add buttons to header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -4,14 +4,45 @@
       <ion-buttons slot="start">
         <ion-menu-button color="primary"></ion-menu-button>
       </ion-buttons>
-      <ion-title>{{ text }}</ion-title>
+      <ion-title class="header-title">{{ text }}</ion-title>
+      <ion-buttons slot="end">
+        <ion-button @click="goBack">
+          <ion-icon slot="icon-only" :icon="arrowBackOutline" />
+        </ion-button>
+        <ion-button @click="goHome">
+          <ion-icon slot="icon-only" :icon="homeOutline" />
+        </ion-button>
+      </ion-buttons>
     </ion-toolbar>
   </ion-header>
 </template>
 
 <script setup>
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonButtons, IonButton, IonHeader, IonMenuButton, IonTitle, IonToolbar, IonIcon } from '@ionic/vue';
+import { arrowBackOutline, homeOutline } from 'ionicons/icons';
+import { useRouter } from 'vue-router';
+
 const props = defineProps({
   text: { type: String, default: '' },
-})
+});
+
+const router = useRouter();
+
+function goBack() {
+  if (window.history.length > 1) {
+    router.back();
+  } else {
+    router.push('/');
+  }
+}
+
+function goHome() {
+  router.push('/');
+}
 </script>
+
+<style scoped>
+.header-title {
+  font-size: 0.95rem;
+}
+</style>
--- src/views/GamePage/GamePage.vue
+++ src/views/GamePage/GamePage.vue
@@ -1,13 +1,6 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-menu-button color="primary"></ion-menu-button>
-        </ion-buttons>
-        <ion-title>Kościany Poker</ion-title>
-      </ion-toolbar>
-    </ion-header>
+    <PageHeader text="Kościany Poker" />
 
     <ion-content :fullscreen="true">
       <div class="game-container">
@@ -90,6 +83,7 @@ import {
 } from '@ionic/vue';
 import { reactive, ref } from 'vue';
 import Dice from './Dice.vue';
+import PageHeader from '../../components/PageHeader.vue';
 
 const players = reactive([
   { id: 1, dice: getEmptyDice(), rollsLeft: 2, turnEnded: false, points: 0 },
